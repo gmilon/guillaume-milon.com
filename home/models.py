552e0914831d653c6b8e432e4ec3fe99b7f0b365
guillaume <gui.milon@gmail.com>
2017-10-12 23:23:44 +0100

    add thme and make header admin, there is still some problems with statics files

--- home/models.py
+++ home/models.py
@@ -1,9 +1,28 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
-    pass
+
+    header_img = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    header_title = models.CharField(null=True, blank=True, max_length=200)
+
+    header_subtitle = models.CharField(null=True, blank=True, max_length=200)
+
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('header_img'),
+        FieldPanel('header_title'),
+        FieldPanel('header_subtitle'),
+    ]
