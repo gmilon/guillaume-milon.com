d5679a36bf7b9703041d1afd29fbbfabe97dcc47
Guillaume <guillaume.milon@activkonnect.com>
2017-10-13 16:34:04 +0200

    now add socials logos and can change Banner BG in wagtail admin

--- home/models.py
+++ home/models.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import, unicode_literals
 
 from django.db import models
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
@@ -17,12 +18,45 @@ class HomePage(Page):
         related_name='+'
     )
 
+    header_bg = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
     header_title = models.CharField(null=True, blank=True, max_length=200)
 
     header_subtitle = models.CharField(null=True, blank=True, max_length=200)
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('header_img'),
+        ImageChooserPanel('header_bg'),
         FieldPanel('header_title'),
         FieldPanel('header_subtitle'),
+        InlinePanel('socials', label="Socials Media"),
     ]
+
+
+class SocialMedia(Orderable):
+    LINKDN = 'icon-linkedin2'
+    FB = 'icon-facebook2'
+    TW = 'icon-twitter2'
+    LOGOS_CLASSES = [
+        (LINKDN, 'linkedn'),
+        (FB, 'Facebook'),
+        (TW, 'Twitter'),
+    ]
+    page = ParentalKey(HomePage, related_name='socials')
+    title = models.CharField(blank=True, max_length=250)
+    logo_class = models.CharField(blank=True, choices=LOGOS_CLASSES, max_length=250)
+    logo_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+    url = models.URLField(blank=True, null=True)
+
