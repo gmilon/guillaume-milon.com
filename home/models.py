02231053aa53c886508255c3bd955c4c9a0c72e1
Guillaume <guillaume.milon@activkonnect.com>
2017-10-13 17:17:02 +0200

    add 2 fields

--- home/models.py
+++ home/models.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailcore.fields import RichTextField
 
 from wagtail.wagtailcore.models import Page, Orderable
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
@@ -17,7 +18,6 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-
     header_bg = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
@@ -25,16 +25,25 @@ class HomePage(Page):
         on_delete=models.SET_NULL,
         related_name='+'
     )
-
     header_title = models.CharField(null=True, blank=True, max_length=200)
-
     header_subtitle = models.CharField(null=True, blank=True, max_length=200)
+
+    # About Me Section
+    about_title = models.CharField(null=True, blank=True, max_length=300)
+    about_text = RichTextField(blank=True)
+
+    # My Resume
+    mr_title = models.CharField(null=True, blank=True, max_length=300)
+    mr_text = RichTextField(blank=True)
+
 
     content_panels = Page.content_panels + [
         ImageChooserPanel('header_img'),
         ImageChooserPanel('header_bg'),
         FieldPanel('header_title'),
         FieldPanel('header_subtitle'),
+        FieldPanel('about_title'),
+        FieldPanel('about_text', classname="full"),
         InlinePanel('socials', label="Socials Media"),
     ]
 
